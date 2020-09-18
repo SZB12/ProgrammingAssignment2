666fba6395c2b102d334d2d77e493ca96b563903
SZB12 <soph235@gmail.com>
2020-09-17 22:58:46 -0700

    getting ready to push file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The makeCacheMatrix and cacheSolve functions take a matrix (matrix must be 
+# invertible), then calculate and cache its inverse. If the inverse of the 
+# matrix has been already cached, the inverted matrix is retrieved and is not 
+# recalculated. Either way, the inverted matrix is returned. 
 
-## Write a short comment describing this function
+# The makeCacheMatrix function takes in an invertible matrix, x, and returns a 
+# list of four functions and stores x and Inv, which is the inverse of x. The 
+# super-assignment operator (<<-) is used to store x and Inv in the parent 
+# environment so that both can be retrieved without recalculation.   
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  Inv <- NULL
+  set <- function(z) {
+    x <<- z
+    Inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(i) Inv <<- i
+  getInv <- function() Inv
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve function takes a list of functions that were created by the 
+# makeCacheMatrix function as arguments, then first gets the stored inverted 
+# matrix, Inv. If the inverse has not been calculated, Inv is Null and it is 
+# then calculated, cached and returned. If the inverted matrix has been 
+# calculated previously, it is retrieved from the cache with a message and 
+# returned without recalculation. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMatrix, ...) {
+  Inv <- cacheMatrix$getInv()
+  if(!is.null(Inv)) {
+    message("getting cached data")
+    return(Inv)
+  }
+  data <- cacheMatrix$get()
+  Inv <- solve(data, ...)
+  cacheMatrix$setInv(Inv)
+  Inv
 }
